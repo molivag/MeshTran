8f405450dc8ac4ac82c93cb1f016c4434aca289e
molivag <mogeofisico@gmail.com>
2026-02-09 18:11:37 +0100

    Se ajusta la ruta de los archivos de entrada para el script python

--- plot_inputs.py
+++ plot_inputs.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 
 # ---------- CARGA DATOS ----------
 # UTM
-dem_utm   = np.loadtxt("./input_data/dem_utm_km.dat")
-sites_utm = np.loadtxt("./input_data/sites_utm_km.dat", ndmin=2)
+dem_utm   = np.loadtxt("./input_data/pyplots/dem_utm_km.dat")
+sites_utm = np.loadtxt("./input_data/pyplots/sites_utm_km.dat", usecols=(1,2), ndmin=2)
 
 
 # Maya / FEMTIC local
 dem_maya   = np.loadtxt("./input_data/topography_for.dat")
-sites_maya = np.loadtxt("./input_data/sites_coord_elev.dat", ndmin=2)
+sites_maya = np.loadtxt("./input_data/sites_coord_elev.dat", usecols=(1,2,3), ndmin=2)
 domain     = np.loadtxt("./input_data/analysis_domain.dat")
-coast      = np.loadtxt("./input_data/coast_line_for.dat", skiprows=1)
+coast      = np.loadtxt("./input_data/coast_line.dat", skiprows=1)
 
 
 resample = 1            # <-- aquí cambias 3, 5, 10, etc.
@@ -33,7 +33,7 @@ ax.scatter(dem_utm[::resample,0],
 
 ax.scatter(sites_utm[:,0],
            sites_utm[:,1],
-           s=ms, marker="v", c="r")
+           s=ms, marker="v", c="m")
 
 ax.set_title("UTM — DEM + Sites")
 ax.set_xlabel("X (km)")
@@ -50,7 +50,7 @@ ax.scatter(dem_maya[::resample,0],
 
 ax.scatter(sites_maya[:,0],
            sites_maya[:,1],
-           s=ms, marker="v", c="r")
+           s=ms, marker="v", c="m")
 
 # Analysis domain (rectángulo)
 xmin, xmax = domain[0,0], domain[0,1]
@@ -59,7 +59,7 @@ ax.plot([xmin, xmax, xmax, xmin, xmin],
         [ymin, ymin, ymax, ymax, ymin], 'k-')
 
 # Coastline
-ax.plot(coast[:,0], coast[:,1], 'b')
+ax.plot(coast[:,0], coast[:,1], 'r')
 
 ax.set_title("Maya / FEMTIC — DEM + Sites + Domain + Coast")
 ax.set_xlabel("X (km)")
