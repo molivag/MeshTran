13a24b5c9557552cf22294da17746d8a1e63b892
molivag <mogeofisico@gmail.com>
2026-05-22 23:51:39 +0200

    geo_utils - creation of module geo_utils now here lives the convertion latlon to utm routine

---
+++ src/geo_utils.f90
@@ -0,0 +1,90 @@
+module geo_utils
+  
+  use mesh_config, only: dp
+  implicit none
+  
+  
+  contains
+
+!=========================================================
+!=======
+!=========================================================trendline
+   subroutine lat_long_to_UTM_km(lat, lon, n_points, east_km, north_km)
+
+      implicit none
+      integer, intent(in)     :: n_points
+      real(dp), intent(in)    :: lat(n_points), lon(n_points)             ! grados decimales
+      ! real(dp), intent(out)   :: east_km(n_points), north_km(n_points)    ! ya en kilometros
+      real(dp), allocatable, intent(out)   :: east_km(:), north_km(:)    ! ya en kilometros
+
+      integer:: i, zone
+      real(dp)               :: x(n_points), y(n_points)                 ! metros
+      real(dp):: lon_mean
+
+      ! --- constantes---
+      real(dp), parameter:: semieje = 6378137.0d0
+      real(dp), parameter:: f = 1.0d0/298.257223563d0
+      real(dp), parameter:: k0 = 0.9996d0
+      real(dp), parameter:: pi = 3.141592653589793d0
+
+      real(dp):: e2, ep2
+      real(dp):: latr(n_points), lonr(n_points), lon0r
+      real(dp):: N(n_points), T(n_points), C(n_points), A(n_points), M(n_points)
+      real(dp):: lon0
+
+      ! --- elipsoide---
+      e2 = f*(2.0d0 - f)
+      ep2 = e2/(1.0d0 - e2)
+
+      ! --- zona UTM---
+      lon_mean = sum(lon)/dble(n_points)
+      zone = int((lon_mean + 180.0d0)/6.0d0) + 1
+      lon0 = (zone - 1)*6.0d0 - 180.0d0 + 3.0d0
+
+      do i = 1, n_points
+         ! --- radianes---
+         latr(i) = lat(i)*pi/180.0d0
+         lonr(i) = lon(i)*pi/180.0d0
+         lon0r = lon0*pi/180.0d0
+
+         ! --- términos auxiliares---
+         N(i) = semieje/sqrt(1.0d0 - e2*sin(latr(i))**2)
+         T(i) = tan(latr(i))**2
+         C(i) = ep2*cos(latr(i))**2
+         A(i) = cos(latr(i))*(lonr(i) - lon0r)
+
+         ! --- arco meridional---
+         M(i) = semieje*((1.0d0 - e2/4.0d0 - 3.0d0*e2**2/64.0d0 - 5.0d0*e2**3/256.0d0)*latr(i) &
+                         - (3.0d0*e2/8.0d0 + 3.0d0*e2**2/32.0d0 + 45.0d0*e2**3/1024.0d0)*sin(2.0d0*latr(i)) &
+                         + (15.0d0*e2**2/256.0d0 + 45.0d0*e2**3/1024.0d0)*sin(4.0d0*latr(i)) &
+                         - (35.0d0*e2**3/3072.0d0)*sin(6.0d0*latr(i)))
+
+         ! --- coordenadas UTM---
+         x(i) = k0*N(i)*(A(i) + (1.0d0 - T(i) + C(i))*A(i)**3/6.0d0 &
+                         + (5.0d0 - 18.0d0*T(i) + T(i)**2 + 72.0d0*C(i) - 58.0d0*ep2)*A(i)**5/120.0d0) + 500000.0d0
+
+         y(i) = k0*(M(i) + N(i)*tan(latr(i))*(A(i)**2/2.0d0 &
+                                              + (5.0d0 - T(i) + 9.0d0*C(i) + 4.0d0*C(i)**2)*A(i)**4/24.0d0 &
+                                              + (61.0d0 - 58.0d0*T(i) + T(i)**2 + 600.0d0*C(i) - 330.0d0*ep2)*A(i)**6/720.0d0))
+
+         ! hemisferio sur
+         !to avoind negative coordinates in northing
+         if (lat(i) < 0.0d0) y(i) = y(i) + 10000000.0d0
+      end do
+
+      !Covierto UTM en metros a UTM en kilometros
+      ! x = x/1000.0d0
+      ! y = y/1000.0d0
+      ALLOCATE(east_km(n_points), north_km(n_points))
+
+      north_km = y/1000.0d0    ! Y = Norte
+      east_km = x/1000.0d0    ! X = Este
+
+      !Lo ideal es que esta conversion se haga fuera e la rutina, al igual en read_dem o read_EDIs
+      ! x = north_km   ! X = Norte
+      ! y = east_km    ! Y = Este
+
+   end subroutine lat_long_to_UTM_km
+
+end module geo_utils
+
